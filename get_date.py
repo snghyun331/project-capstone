9722ce96fd2e2dd07e0e04076a9599b4cf75eac1
Lee SH <snghyun331@gmail.com>
2023-05-21 23:43:09 +0900

    get all date

---
+++ get_date.py
@@ -0,0 +1,28 @@
+from elasticsearch import Elasticsearch
+import warnings
+warnings.filterwarnings('ignore')
+
+def get_all_date():
+    
+    es = Elasticsearch("https://118.67.134.52:9200",http_auth=("elastic", "elastic"),verify_certs= False,
+        http_compress= False)
+
+    res = es.search(index = 'platform_sales',scroll = '1m', size = 10000, body={"query": {"match_all": {}}})
+    res_hits = res['hits']['hits']
+    scroll_id = res['_scroll_id']
+
+    new_docs = []
+    while len(res_hits) > 0:
+        for res_hit in res_hits:
+            docs = res_hit['_source']
+            sold_at = docs['sold_at']
+            new_docs.append(sold_at)
+            
+        res = es.scroll(scroll_id = scroll_id, scroll = '1m')   # # 이전 검색 결과와 동일한 scroll_id를 사용하여 다음 검색 결과를 가져옴
+        res_hits = res['hits']['hits']
+        
+    return new_docs
+
+
+
+# len(new_docs)  480597
