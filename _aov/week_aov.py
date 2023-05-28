a6958b55aa7c9da9a24ddf70ab5ec4714a67df71
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-05-28 23:33:51 +0900

    Update week_aov.py
    주간 객단가 인덱스에 저장까지 구현했습니다.

--- _aov/week_aov.py
+++ _aov/week_aov.py
@@ -1,6 +1,7 @@
 from elasticsearch import Elasticsearch 
 from dateutil import parser
-
+import datetime
+    
 # 조회하는 데이터의 날짜의 1주일치 데이터 가져와서 객단가 평균 구하기 
 def make_week_AOV(date, store_id):
     
@@ -43,38 +44,16 @@ def make_week_AOV(date, store_id):
             
     week_aov = int(sum(unit_price) / len(unit_price))
     
-    
     # insert한 데이터 날짜 기준 
     # start_date = date(year, month, day - 7)
     # end_date = date(year, month, day - 1)
     # store_id ## 데이터의 가게 고유 번호 
-    return week_aov
+    data = {
+        "store_id": store_id,
+        "start_date": datetime.date(year, month, day - 7),
+        "end_date": datetime.date(year, month, day - 1),
+        "week_unit_price": week_aov
+    }   
 
-# id를 받아서 date값과 store_id값을 추출하여 make_week_aov함수의 인자로 사용
-def Week_AOV(id): 
-    es = Elasticsearch(
-    hosts='https://118.67.134.52:9200',
-    http_auth=("elastic", "elastic"),   
-    verify_certs= False,
-    http_compress= False
-    )
-    
-    index = 'platform_sales' 
-    body = {
-            "size": 10000,
-            "query": { "match" : {"id" : id}}
-           }
-    res = es.search(index = index, body = body)
-    
-    res_hits = res["hits"]["hits"]
-    for arr in res_hits:
-        res_source = dict(arr)
-        res_sold_at = res_source["_source"]["sold_at"]
-        
-        res_date = parser.parse(res_sold_at).date() # res_sold_at을 날짜만 가져옴
-        res_store_id = res_source["_source"]["store_id"] # store_id값 추출
-        
-    return make_week_AOV(res_date, res_store_id)
-    
-#print(Week_AOV(5555)) 
-## platform_sales_per_price 인덱스에서 id가 5555번인 데이터와 store_id가 같은 객단가를 7일(1주)전의 데이터까지 평균낸 값 리턴 
+    es.index(index="platform_sales_week_per_price", doc_type="_doc", body=data)
+    print("done")
