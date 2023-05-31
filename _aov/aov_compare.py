0193a9d08d378d86f5c34d9540286816e40f6f49
Lee SH <snghyun331@gmail.com>
2023-06-01 00:23:58 +0900

    modify2 aov_compare.py

--- _aov/aov_compare.py
+++ _aov/aov_compare.py
@@ -1,6 +1,15 @@
-from elasticsearch import Elasticsearch 
+from datetime import datetime
+from dateutil import parser
+from elasticsearch import Elasticsearch
 
-def compare(unit_price):
+def compare(amount_sale,store_id, start_date, end_date):
+    start_year = start_date[:4]
+    start_month = start_date[4:6]
+    start_day = start_date[6:]
+    end_year = end_date[:4]
+    end_month = end_date[4:6]
+    end_day = end_date[6:]
+    
     es = Elasticsearch(
             hosts='https://118.67.134.52:9200',
             http_auth=("elastic", "elastic"),   
@@ -9,13 +18,27 @@ def compare(unit_price):
         )
     
     index = 'platform_sales_week_per_price' 
-    body = {"query": {"match_all": {}}}
+    body = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"term": {"store_id": store_id}},
+                        {"range": {
+                            "sold_at": {
+                                "gte": parser.parse(f"{start_year}-{start_month}-{start_day}T00:00:00+00:00"),
+                                "lte": parser.parse(f"{end_year}-{end_month}-{end_day}T23:59:59+00:00")
+                            }
+                        }}
+                    ]
+                }
+            }
+        }
     res = es.search(index = index, body = body, size = 10000)
     res_hits = res["hits"]["hits"]
     docs = res_hits['_source']
     week_unit_price = docs["week_unit_price"]
     
-    if week_unit_price * 2 < unit_price:   # 일주일 객단가 > insert 데이터 객단가 이면 true 반환
+    if week_unit_price*2 < amount_sale:
         return True
     else:
         return False
