323a0bd1e81d234dae7281c213480f8bfb10c7b4
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-06-04 10:45:55 +0900

    Update aov_compare.py

--- elastic/aov_compare.py
+++ elastic/aov_compare.py
@@ -4,15 +4,11 @@ from elasticsearch import Elasticsearch
 
 import conn
 
-def compare(card_number, amount_sale, store_id, start_date, end_date):
-    start_year = start_date[:4]
-    start_month = start_date[4:6]
-    start_day = start_date[6:]
-    end_year = end_date[:4]
-    end_month = end_date[4:6]
-    end_day = end_date[6:]
-    
-    
+def compare(card_number, amount_sale, store_id, start_date):
+    start_year = start_date.year
+    start_month = start_date.month
+    start_day = start_date.day
+   
     es = conn.Conn()
     
     # 주간 객단가 가져오기 
@@ -39,7 +35,7 @@ def compare(card_number, amount_sale, store_id, start_date, end_date):
                         {"range": {
                             "sold_at": {
                                 "gte": parser.parse(f"{start_year}-{start_month}-{start_day}T00:00:00+00:00"),
-                                "lte": parser.parse(f"{end_year}-{end_month}-{end_day}T23:59:59+00:00")
+                                "lte": parser.parse(f"{start_year}-{start_month}-{start_day}T23:59:59+00:00")
                             }
                         }}
                     ]
@@ -56,16 +52,11 @@ def compare(card_number, amount_sale, store_id, start_date, end_date):
     
     # platform_sales에서 amount_sale값 가져오기
     res2_hits = res2["hits"]["hits"] 
-
-    # 비교하는 데이터의 card_number가 같은 amount_sale값의 리스트
-    info = [] 
-    
+    info = [] # 비교하는 데이터의 card_number가 같은 amount_sale값의 리스트
     for arr in res2_hits:
         res2_source = dict(arr)
-        # 현재 es에 저장된 데이터의 card_number들
-        current_card_number = res2_source["_source"]["card_number"] 
-        # 비교하는 데이터와 카드 번호가 같을 경우 리스트에 추가
-        if card_number == current_card_number: 
+        current_card_number = res2_source["_source"]["card_number"] # 현재 es에 저장된 데이터의 card_number들
+        if card_number == current_card_number: # 비교하는 데이터와 카드 번호가 같을 경우 리스트에 추가
             info.append(res2_source["_source"]["amount_sale"])
       
     # 가장 최근의 3개의 amount_sale의 값이 모두 같은 값이면 변수 outlier는 true
@@ -81,13 +72,17 @@ def compare(card_number, amount_sale, store_id, start_date, end_date):
             break
   
     
-    if week_unit_price*2 < amount_sale and outlier == True:
+    if week_unit_price*2 < amount_sale or outlier == True:
         return True
     else:
         return False
     
-# df = compare("5461-11**-****-7537", 32000, 56, "20220202", "20220202")
+# df = compare("5461-11**-****-7537", 32000, 56, "20220202")
 # 예시) 카드 번호가 5461-11**-****-7537인 고객이 56번 가게에 32000원씩 3번 이상 결제해서 True가 나옴  
+
+
+
+
 
 
 """ 이전 
