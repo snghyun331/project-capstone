3e2f80701826083e0e8a14287569213b62b64df7
Lee SH <snghyun331@gmail.com>
2023-06-26 19:26:54 +0900

    Modify app.py

--- apis/app.py
+++ apis/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from elasticsearch import helpers
+from elasticsearch import helpers, Elasticsearch
 from dateutil import parser
 from datetime import datetime
 import warnings
@@ -7,7 +7,13 @@ warnings.filterwarnings('ignore')
 # import os
 # import sys 
 # sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-import elastic.conn as conn
+
+es = Elasticsearch(
+    hosts='https://118.67.134.52:9200',
+    http_auth=("elastic", "elastic"),   
+    verify_certs= False,
+    http_compress= False
+    )
 
 
 app = Flask(__name__)
@@ -18,33 +24,17 @@ def home():
     return "엘라스틱 서치를 플라스크로 성능 향상 시키기"
 
 
-# request parameter에서 start_date, end_date 값을 받아서 해당 범위의 객단가 정보 가져오기
+# request parameter에서 date 값을 받아서 해당 범위의 객단가 정보 가져오기
 @app.route('/search-unitprice', methods = ['GET'])
 def search():
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
-    # url 예시: http://localhost:5000/search?start_date=20230501&end_date=20230531
-    
-    start_year = start_date[:4]
-    start_month = start_date[4:6]
-    start_day = start_date[6:]
-    end_year = end_date[:4]
-    end_month = end_date[4:6]
-    end_day = end_date[6:]
-    
-    es = conn.Conn()
+    date = request.args.get('date')
+    # url 예시: http://localhost:5000/search-unitprice?date=20230501
+    year = date[:4]
+    month = date[4:6]
+    day = date[6:]
+
     index = "platform_sales_per_price"
-    body = {"query":
-                {"range":
-                    {"sold_at": 
-                        {
-                        "gt":parser.parse(f"{start_year}-{start_month}-{start_day}T00:00:00+00:00"), 
-                        "lt":parser.parse(f"{end_year}-{end_month}-{end_day}T23:59:59+00:00")
-                        }
-                    }
-                }
-        }
-    
+    body = {"query": {"match_all": {}}}
     res = es.search(index = index, scroll = '1m', size = 10000, body = body)
     res_hits = res["hits"]["hits"]
     scroll_id = res['_scroll_id']
@@ -66,43 +56,45 @@ def search():
                 'total_sale': total_sale,
                 'unit_price': unit_price
             }
-            
-            new_docs.append(info)
+            if date == f"{year}-{month}-{day}":
+                new_docs.append(info)
             
         res = es.scroll(scroll_id = scroll_id, scroll = '1m')   
         res_hits = res['hits']['hits']
     
     return jsonify(new_docs) 
-#     output: [ 
-#                 {'store_id':12, 'date': 2020-01-01...},
-#                 {'store_id':13, 'date': 2020..}  
-#             ]
 
 
 
-# 일주일 평균 객단가를 가져오기
-@app.route('/search-weekaov')
+# 특정 가게의 일주일 평균 객단가를 가져오기
+@app.route('/search-weekaov',methods = ['GET'])
 def Week_AOV(): 
-    es = conn.Conn()
-    
+    storeId = request.args.get('store_id')
+    # url 예시: http://localhost:5000/search-weekaov?store_id=50
+    new_docs = []
     index = 'platform_sales_week_per_price' 
     body = {"query": {"match_all": {}}}
     res = es.search(index = index, body = body, size = 10000)
     res_hits = res["hits"]["hits"]
-    docs = res_hits['_source']
-    store_id = docs['store_id']
-    start_date = docs['start_date']
-    end_date = docs['end_date']
-    week_unit_price = docs['week_unit_price']
-        
-    return  f"""
-    <h4>store_id: {store_id}</h4>
-    <h4>start_date: {start_date}</h4>
-    <h4>end_date: {end_date}</h4>
-    <h4>week_unit_price: {week_unit_price}</h4>
-    """
+    for res_hit in res_hits:  # res_hits[0], res_hits[1]....
+        docs = res_hit['_source']
+        store_id = docs['store_id']
+        start_date = docs['start_date']
+        end_date = docs['end_date']
+        week_unit_price = docs['week_unit_price']
+        info = {
+                'store_id': store_id,
+                'start_date': start_date,
+                'end_date': end_date,
+                'week_unit_price': week_unit_price,
+            }
+        if store_id == int(storeId):
+            new_docs.append(info)
+        else:
+            pass
+    
+    return jsonify(new_docs) 
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
-    
+    app.run(host = '0.0.0.0', port = 3000)
